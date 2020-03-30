72a5dda719ebb343fc4d1696c6590c09ad9fe102
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-03-30 17:30:54 +0300

    Function my_select added

--- enumerables.rb
+++ enumerables.rb
@@ -6,8 +6,9 @@ module Enumerable
         yield(self[i])
       end
       self
+    else
+      to_enum(:my_each)
     end
-    self
   end
 
   def my_each_with_index
@@ -17,21 +18,29 @@ module Enumerable
         yield(self[i], i)
       end
       self
+    else
+      to_enum(:my_each_with_index)
     end
-    self
+  end
+
+  def my_select
+    if block_given?
+      arr_result = []
+      length = self.length
+      length.times do |i|
+        arr_result << self[i] if yield(self[i])
+      end
+      return arr_result
+    else
+      to_enum(:my_select)
+    end
+  end
+
+  def my_all?
+    
   end
 end
 
-numbers = (1..5).to_a
-result = numbers.my_each do |x|
-  puts x > 3
-end
+filtered = (1..6).to_a.my_select
 
-print result
-
-result = numbers.my_each_with_index do |x, i|
-  puts x > 3
-  puts i
-end
-
-print result
+print filtered
