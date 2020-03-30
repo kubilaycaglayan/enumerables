be0820f82f19779c168b2d3beddf972c2089136e
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-03-31 00:49:38 +0300

    Function my_inject added

--- enumerables.rb
+++ enumerables.rb
@@ -1,5 +1,4 @@
-# rubocop:disable Style/CaseEquality
-
+# rubocop:disable Style/CaseEquality, Metrics/ModuleLength
 module Enumerable
   def my_each
     if block_given?
@@ -112,22 +111,57 @@ module Enumerable
     end
     length
   end
+
   # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def my_map
-    length = self.size
+    length = size
     result = []
     if block_given?
       length.times do |i|
-        result << yield(self.to_a[i])
+        result << yield(to_a[i])
       end
       result
     else
       to_enum(:my_map)
     end
   end
-end
 
-# rubocop:enable Style/CaseEquality
+  def my_inject(initial = nil, sym = nil)
+    calculation = {
+      :+ => proc { |x, y| x + y },
+      :- => proc { |x, y| x - y },
+      :/ => proc { |x, y| x / y },
+      :* => proc { |x, y| x * y }
+    }
+    length = size
+    if block_given?
+      if initial
+        accumulation = initial
+        length.times do |i|
+          accumulation = yield(accumulation, to_a[i])
+        end
+      else
+        accumulation = to_a[0]
+        (length - 1).times do |i|
+          accumulation = yield(accumulation, to_a[i + 1])
+        end
+      end
+      accumulation
+    elsif initial.is_a? Symbol
+      accumulation = to_a[0]
+      (length - 1).times do |i|
+        accumulation = calculation[initial].call(accumulation, to_a[i + 1])
+      end
+    else
+      accumulation = initial
+      length.times do |i|
+        accumulation = calculation[sym].call(accumulation, to_a[i])
+      end
+    end
+    accumulation
+  end
+end
+# rubocop:disable Lint/ParenthesesAsGroupedExpression, Lint/AmbiguousBlockAssociation
 def my_test
   puts '------> my_any?'.upcase
   puts(%w[ant bear cat].my_any? { |word| word.length >= 3 }) #=> true
@@ -146,9 +180,30 @@ def my_test
   puts [nil, false].my_none? #=> true
   puts [nil, false, true].my_none? #=> false
   puts '-------> my_map?'.upcase
-  print (1..4).my_map { |i| i*i }      #=> [1, 4, 9, 16]
+  print (1..4).my_map { |i| i * i } #=> [1, 4, 9, 16]
   puts
-  print (1..4).my_map { "cat"  }   #=> ["cat", "cat", "cat", "cat"]
+  print (1..4).my_map { 'cat' } #=> ["cat", "cat", "cat", "cat"]
   puts
+  puts '-------> my_inject?'.upcase
+  result = %w[a b cc].my_inject('x') do |memo, n|
+    memo + n
+  end
+  puts result
+  # Sum some numbers
+  puts (5..10).my_inject(:+) #=> 45
+  # Same using a block and inject
+  puts (5..10).my_inject { |sum, n| sum + n } #=> 45
+  # Multiply some numbers
+  puts (5..10).my_inject(1, :*) #=> 151200
+  # Same using a block
+  puts (5..10).my_inject(1) { |product, n| product * n } #=> 151200
+  # find the longest word
+  longest = %w[cat sheep bear].my_inject do |memo, word|
+    memo.length > word.length ? memo : word
+  end
+  puts longest #=> "sheep"
+  puts (1..5).my_inject(:*)
 end
 my_test
+
+# rubocop:enable Style/CaseEquality, Metrics/ModuleLength, Lint/ParenthesesAsGroupedExpression, Lint/AmbiguousBlockAssociation
