78cb526752975482dddb4725873f5a703327911a
Ikraam <ikraamghoor@gmail.com>
2020-04-08 16:45:40 +0200

    Tested my_each

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,18 +2,25 @@ require './enumerables.rb'
 
 RSpec.describe Enumerable do
   include Enumerable
+  let(:arr) { [1, 2, 3] }
 
-  describe "#sum" do
-    it "returns the sum of a and b" do
+  describe '#sum' do
+    it 'returns the sum of a and b' do
       expect(sum(3, 9)).to eql(12)
     end
   end
 
-  describe "#my_each" do
-    it "returns the range as an array" do
-      expect(3.my_each { |x| x } ).to eql(3)
+  describe '#my_each' do
+    it 'returns the range as an array' do
+      expect(arr.my_each { |x| x * 3 }).to eql(arr.each { |x| x * 3 })
+    end
+
+    it 'returns an enumerator if no block is given' do
+      expect(arr.my_each).to be_a(Enumerator)
+    end
+
+    it 'should throw an argument error if passed an argument' do
+      expect { arr.my_each('whatever') }.to raise_error(ArgumentError)
     end
   end
 end
-
-
